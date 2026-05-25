f6a41c889cce0c0db5e7f37d937755cbd11c8136
LalonSheikh <abusalekmdlalonsheikh@gmail.com>
2026-05-26 00:02:00 +0600

    for vercel something changes    added

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,6 +1,18 @@
-export async function getAllBooks() {
-  const res = await fetch("http://localhost:3000/books.json");
-  const data = await res.json();
+// export async function getAllBooks() {
+//   const res = await fetch("http://localhost:3000/books.json");
+//   const data = await res.json();
 
-  return data;
+//   return data;
+// }
+// export async function getAllBooks() {
+//   const res = await fetch("/books.json");
+//   const data = await res.json();
+
+//   return data;
+// }
+export async function getAllBooks() {
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+
+  const res = await fetch(new URL("/books.json", baseUrl));
+  return res.json();
 }
